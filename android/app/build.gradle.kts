8a6e32cb1952d84ed8d451312c9dc43fa25d79b6
RexVane <rexvane@example.com>
2026-07-14 23:09:23 +0800

    chore(release): v1.2.0 版本号与 README 同步(三平台)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.rexvane.inkhole"
         minSdk = 24
         targetSdk = 34
-        versionCode = 14
-        versionName = "1.1.0"
+        versionCode = 15
+        versionName = "1.2.0"
     }
 
     buildTypes {
